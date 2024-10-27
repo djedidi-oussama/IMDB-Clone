266a3fd85cd560d28a573012614e2bf462b69eaf
djedidi-oussama <djdidoussama@gmail.com>
2024-10-27 23:07:09 +0100

    fix error

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ async function page({ searchParams }) {
     const res = await fetch(
       `https://api.themoviedb.org/3/${
         genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
-      }?api_key=${API_KEY}&language=en-US&page=1`
+      }?api_key=${API_KEY}&language=en-US&page=2`
     );
 
     if (!res.ok) {
